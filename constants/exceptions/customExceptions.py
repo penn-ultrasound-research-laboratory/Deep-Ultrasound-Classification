41d74b9765743288c615b5a89a5da8c54db4085a
goodmattg <mattgoodman13@gmail.com>
2018-07-27 16:55:03 -0700

    Full numpy dataset generation/save working

--- constants/exceptions/customExceptions.py
+++ constants/exceptions/customExceptions.py
@@ -1,2 +1,5 @@
 class PatientSampleGeneratorException(Exception):
     """Raise for error instantiating PatientSampleGenerator due to malformatted input data"""
+
+class ExtractSavePatientFeatureException(Exception):
+    """Error extracting and saving patient feature to file"""
