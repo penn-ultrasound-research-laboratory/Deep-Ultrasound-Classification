a6a463ac3a1919f73163d855216ffcfd74827af5
goodmattg <mattgoodman13@gmail.com>
2018-08-19 17:55:14 -0700

    refactored process ultrasound images to run OCR and segmentation as two stages with fault tolerance if OCR fails. Also switched to standard library logging

--- ProcessUltrasoundImages.py
+++ ProcessUltrasoundImages.py
@@ -11,6 +11,7 @@ from constants.ultrasoundConstants import (
     HSV_GRAYSCALE_THRESHOLD,
     IMAGE_TYPE,
     IMAGE_TYPE_LABEL,
+    TIMESTAMP_LABEL,
     TUMOR_BENIGN,
     TUMOR_MALIGNANT,
     TUMOR_TYPES,
@@ -29,7 +30,61 @@ import uuid
 
 logger = logging.getLogger('processing')
 
-def process_patient_ocr(
+def process_patient_segmentation(
+    path_to_frame,
+    absolute_path_to_focus_output_directory,
+    image_type):
+
+    if image_type is IMAGE_TYPE.COLOR:
+
+        hash_path = get_color_image_focus(
+            path_to_frame, 
+            absolute_path_to_focus_output_directory, 
+            np.array(HSV_COLOR_THRESHOLD.LOWER.value, np.uint8), 
+            np.array(HSV_COLOR_THRESHOLD.UPPER.value, np.uint8))
+
+    else: 
+        hash_path = get_grayscale_image_focus(
+            path_to_frame, 
+            absolute_path_to_focus_output_directory, 
+            np.array(HSV_GRAYSCALE_THRESHOLD.LOWER.value, np.uint8), 
+            np.array(HSV_GRAYSCALE_THRESHOLD.UPPER.value, np.uint8))
+
+    return hash_path
+
+
+def process_patient_ocr(color_frame, image_type):
+    # Always use the grayscale converted image for text isolation
+    grayscale_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
+    
+    # Some weird thresholding code. Not sure where it came from
+    grayscale_frame = cv2.threshold(grayscale_frame, 0, 255,
+        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    return isolate_text(grayscale_frame, image_type)
+
+
+    # if self.auto_resize_to_manifest_scale_max:
+    #     try:
+    #         frame_scale = self.patient_frames[self.frame_index][SCALE_LABEL]
+    #         upscale_ratio = self.manifest_scale_max / frame_scale
+
+    #         logging.debug("Upscale Ratio: {}".format(upscale_ratio))
+
+    #         loaded_image = cv2.resize(
+    #             loaded_image, 
+    #             None, 
+    #             fx=upscale_ratio, 
+    #             fy=upscale_ratio, 
+    #             interpolation=cv2.INTER_CUBIC)
+
+    #     except:
+    #         logger.error("Unable to auto-resize. Frame {} does not have scale label".format(
+    #             self.patient_frames[self.frame_index][FOCUS_HASH_LABEL]
+    #         ))
+            
+
+def process_patient(
     absolute_path_to_patient_folder, 
     relative_path_to_frames_folder, 
     relative_path_to_focus_output_folder,
@@ -38,7 +93,7 @@ def process_patient_ocr(
     patient_type_label=None):
     """Process an individual patient
 
-    Each patient"s ultrasound lives in a unique folder. Expectation is that a consistently named folder exists for each patient containing the individual frames of the patient"s ultrasound. For each patient, go frame by frame and process the frf = open("workfile", "w")ame for type (color/grayscale), information (WF, RAD/ARAD, etc.), and a frame focus. The frame focus is clearly delineated for color frames and must be inferred for grayscale frames. 
+    Each patient"s ultrasound lives in a unique folder. Expectation is that a consistently named folder exists for each patient containing the individual frames of the patient"s ultrasound. For each patient, go frame by frame and process use OCR to extract textual information (WF, RAD/ARAD, etc.), and a the tumor segmentation ("frame focus"). The frame focus is clearly delineated for color frames and must be inferred for grayscale frames. 
 
     Arguments:
         absolute_path_to_patient_folder: absolute path to patient folder
@@ -54,7 +109,7 @@ def process_patient_ocr(
         A list of records for all cleanly processed patient frames
     """
 
-    patient_label = os.path.basename(absolute_path_to_patient_folder)
+    # patient_label = os.path.basename(absolute_path_to_patient_folder)
 
     absolute_path_to_frame_directory = "{}/{}".format(
         absolute_path_to_patient_folder.rstrip("/"),
@@ -71,97 +126,106 @@ def process_patient_ocr(
         os.mkdir(absolute_path_to_focus_output_directory)
 
     # Set up frame objects
-    all_frames = [name for name in os.listdir(absolute_path_to_frame_directory)]
-    ocr_clear_frames = []
-
+    individual_patient_frames = [name for name in os.listdir(absolute_path_to_frame_directory)]
+    
     # Array storing all found & cleared text records
-    found_text_records = []
+    compiled_patient_records = []
 
-    #
+
+    ############################################################
     # First pass over all data frames: 
+
     # OCR to get frame scale, RAD/ARAD, etc 
-    #
+    ############################################################
 
-    for frame in all_frames:
-        logger.debug("Attempting text OCR for frame: {}".format(frame))
-        try: 
-            path_to_frame = "{}/{}".format(absolute_path_to_frame_directory, frame)
-            bgr_image = cv2.imread(path_to_frame, cv2.IMREAD_COLOR)
+    for frame in individual_patient_frames:
 
-            # Determine whether the frame is color or grayscale
-            image_type = determine_image_type(bgr_image)
+        path_to_frame = "{}/{}".format(absolute_path_to_frame_directory, frame)
+        color_frame = cv2.imread(path_to_frame, cv2.IMREAD_COLOR)
 
-            # Always use the grayscale converted image for text isolation
-            grayscale_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-            grayscale_image = cv2.threshold(grayscale_image, 0, 255,
-                cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # Determine whether the frame is color or grayscale
+        image_type = determine_image_type(color_frame)
 
-            found_text = isolate_text(grayscale_image, image_type)
+        try:
+            logger.info("Attempting text OCR for frame: {}".format(frame))
+            found_text = process_patient_ocr(color_frame, image_type)
 
+            # Augment the found text with standard frame information
+    
             found_text[FRAME_LABEL] = os.path.basename(path_to_frame)
             found_text[TUMOR_TYPE_LABEL] = patient_type_label
+    
+            if image_type is IMAGE_TYPE.COLOR:
+                found_text[IMAGE_TYPE_LABEL] = IMAGE_TYPE.COLOR.value
+            else:
+                found_text[IMAGE_TYPE_LABEL] = IMAGE_TYPE.GRAYSCALE.value
+
+            compiled_patient_records.append(found_text)
             
-            found_text[IMAGE_TYPE_LABEL] = IMAGE_TYPE.COLOR.value if image_type is IMAGE_TYPE.COLOR else IMAGE_TYPE.GRAYSCALE.value
-
-            ocr_clear_frames.append(frame)
-
         except Exception as e:
-            # Write the specific error in the failure log. Progress to the next frame
-            logger.error("{}\n".format(e))
+            logger.error("Failed text OCR for frame: {}. {}".format(frame, e))
             continue
 
-    if self.auto_resize_to_manifest_scale_max:
-        try:
-            frame_scale = self.patient_frames[self.frame_index][SCALE_LABEL]
-            upscale_ratio = self.manifest_scale_max / frame_scale
 
-            logging.debug("Upscale Ratio: {}".format(upscale_ratio))
-
-            loaded_image = cv2.resize(
-                loaded_image, 
-                None, 
-                fx=upscale_ratio, 
-                fy=upscale_ratio, 
-                interpolation=cv2.INTER_CUBIC)
-
-        except:
-            logger.error("Unable to auto-resize. Frame {} does not have scale label".format(
-                self.patient_frames[self.frame_index][FOCUS_HASH_LABEL]
-            ))
-
-    #
-    # Second pass over frames that cleared OCR: 
+    ############################################################
+    # SECOND pass over frames that cleared OCR: 
+    # 
     # Segmentation to crop the frame's "focus"
     # Ideally, the focus is a close crop of the tumor
-    #
+    ############################################################
 
-    for frame in ocr_clear_frames:    
+    for frame in individual_patient_frames:
+
+        path_to_frame = "{}/{}".format(absolute_path_to_frame_directory, frame)
+        frame_label = os.path.basename(path_to_frame)
+
+        # Check if any record with this path_to_frame exists in the compiled_patient_records. 
+        # If so, OCR was successful and segmentation will be used to update the record. Otherwise, 
+        # a new record will be created
+
         try:
-            if image_type is IMAGE_TYPE.COLOR:
+            logger.info("Attempting tumor segmentation for frame: {}".format(frame))
 
-                hash_path = get_color_image_focus(
-                    path_to_frame, 
-                    absolute_path_to_focus_output_directory, 
-                    np.array(HSV_COLOR_THRESHOLD.LOWER.value, np.uint8), 
-                    np.array(HSV_COLOR_THRESHOLD.UPPER.value, np.uint8))
+            # Get the tumor segmentation from the patient frame            
+            hash_path = process_patient_segmentation(
+                path_to_frame,
+                absolute_path_to_focus_output_directory,
+                image_type)
 
-            else: 
-                hash_path = get_grayscale_image_focus(
-                    path_to_frame, 
-                    absolute_path_to_focus_output_directory, 
-                    np.array(HSV_GRAYSCALE_THRESHOLD.LOWER.value, np.uint8), 
-                    np.array(HSV_GRAYSCALE_THRESHOLD.UPPER.value, np.uint8))
+            frame_record = [rec for rec in compiled_patient_records if rec[FRAME_LABEL] == frame_label]
 
-            found_text[FOCUS_HASH_LABEL] = hash_path
-            found_text_records.append(found_text)
+            # Should be exactly one matching record if OCR cleared.
+            if len(frame_record) > 0:
+                frame_record = frame_record[0] 
+                frame_record[FOCUS_HASH_LABEL] = hash_path
+            else:
+                # OCR did not clear for the frame. Create placeholder record with only frame segementation
+                # Frame record will not contain any extracted text information
+                color_frame = cv2.imread(path_to_frame, cv2.IMREAD_COLOR)
+
+                # Determine whether the frame is color or grayscale
+                image_type = determine_image_type(color_frame)
+
+                frame_record = {}
+                frame_record[FRAME_LABEL] = os.path.basename(path_to_frame)
+                frame_record[TUMOR_TYPE_LABEL] = patient_type_label
+        
+                if image_type is IMAGE_TYPE.COLOR:
+                    frame_record[IMAGE_TYPE_LABEL] = IMAGE_TYPE.COLOR.value
+                else:
+                    frame_record[IMAGE_TYPE_LABEL] = IMAGE_TYPE.GRAYSCALE.value
+
+                frame_record[FOCUS_HASH_LABEL] = hash_path
+
+                # The newly created frame record must be added to the patient's list of records (frames)
+                compiled_patient_records.append(frame_record)
 
         except Exception as e:
-            # Write the specific error in the failure log. Progress to the next frame
-            logger.error("[{}, {}, {}] | {}".format(patient_label, frame, image_type, e))
+            logger.error("Failed tumor segmentation for frame: {}. {}".format(frame, e))
             continue
-
-    return found_text_records
             
+    return compiled_patient_records
+
 
 
 def process_patient_set(
@@ -230,7 +294,7 @@ def process_patient_set(
                 path.rstrip("/"),
                 patient)
 
-            acquired_records = process_patient_ocr(
+            acquired_records = process_patient(
                 patient_directory_absolute_path, 
                 relative_path_to_frames_folder, 
                 relative_path_to_focus_output_folder,
@@ -240,7 +304,7 @@ def process_patient_set(
 
             patient_records[patient] = acquired_records
 
-    patient_records["TIMESTAMP"] = timestamp
+    patient_records[TIMESTAMP_LABEL] = timestamp
 
     # Dump the patient records to file
     json.dump(patient_records, manifest_file)
