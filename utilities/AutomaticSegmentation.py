b4412bbfe292d56ccc5cf9d4070b2b333eadcd83
goodmattg <mattgoodman13@gmail.com>
2018-10-04 21:57:30 -0700

    initial implementation of seed generation. Attempting region selection

--- utilities/AutomaticSegmentation.py
+++ utilities/AutomaticSegmentation.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import logging
+import os
 import uuid
 import math
 import numpy as np
@@ -24,6 +25,7 @@ def __gaussian_filter(image):
     blur = cv2.GaussianBlur(image, (ks, ks), sigma)
 
     return blur
+
 
 def __linear_normalization(image):
     lbound05, ubound95 = np.percentile(image, (5, 95))
@@ -163,26 +165,92 @@ def __get_reference_point(image, max_iters=100, eps=2):
         
     return C[max_iters]
 
+
 def get_ROI(image):
     blur = __gaussian_filter(image)
     normalized = __linear_normalization(blur)
     enhanced = __enhance_hypoechoic_regions(normalized)
-    __get_reference_point(enhanced)
+    reference_point = __get_reference_point(enhanced)
 
-    return
-    # return enhanced
+    return reference_point, enhanced
+
+
+def __unit_flat_kernel(p_diff):
+    return 1.0 if p_diff <= 1.0 else 0.0
+
+
+def __get_seed_point(image, rp, nd=12, h=12, eps=2, max_iters=100):
+    
+    ind = np.indices(image.shape)
+    
+    pre_cands = np.repeat(rp.reshape(1,2), nd, axis=0)
+
+    ext = np.column_stack((
+        np.cos(2*math.pi*np.arange(nd) / nd),
+        np.sin(2*math.pi*np.arange(nd) / nd
+    )))
+
+    H_neg_sqrt = 1 / h
+
+    v_msk = np.vectorize(__unit_flat_kernel)
+
+    img_dot_ind = np.multiply(image, ind)
+    
+    pre_cands = pre_cands + h * ext
+    post_cands = np.empty(pre_cands.shape)
+    max_crit = np.empty(nd)
+    
+    for d in range(nd):
+        p = pre_cands[d].reshape(2,1,1)
+        for it in range(max_iters):
+
+            K_h = v_msk(np.linalg.norm(H_neg_sqrt * (ind - p), axis=0))
+            nc = np.sum(np.multiply(K_h, image).flatten())
+            p_new = np.sum(np.multiply(K_h, img_dot_ind), axis=(1,2)) / nc
+            
+            if np.linalg.norm(p-p_new < eps):
+                p = p_new
+                break
+            
+            p = p_new
+        
+        max_crit[d] = nc
+        post_cands[d] = p
+        
+    i = np.argmax(max_crit)
+    max_p = post_cands[i, :]
+    
+    return max_p, post_cands
 
 
 if __name__ == "__main__":
 
-    elephant = cv2.imread("../TestImages/CroppedImage.png", cv2.IMREAD_GRAYSCALE)
+    n = len(os.listdir("TestImages/bank"))
+    c = 3
+    r = (n // 3) + 1
 
-    get_ROI(elephant)
+    fig = plt.figure(figsize=(15, 15))
 
-    # cv2.imshow("elephant", elephant)
-    # cv2.waitKey(0)
+    for i, f in enumerate(os.listdir("TestImages/bank")):
+        
+        img = cv2.imread("TestImages/bank/{}".format(f), cv2.IMREAD_GRAYSCALE)
+        rp, enh = get_ROI(img)
+        max_p, post_cands = __get_seed_point(enh, rp, nd=12, h=20, eps=1)
+        
+        fig.add_subplot(r, c, i+1)
+        
+        plt.imshow(img)
+        plt.scatter(x=rp[1], y=rp[0], s=40, c='r')
+    #     plt.scatter(x=post_cands[:,1], y=post_cands[:,0], s=20, c='g')
+        plt.scatter(x=max_p[1], y=max_p[0], s=40, c='b')
+        
+    plt.show()
 
-    # # cv2.imshow("enhanced", cv2.applyColorMap(enhanced, cv2.COLORMAP_JET))
-    # # cv2.waitKey(0)
-    # plt.imshow(enhanced)
-    # plt.show()
+    # se = np.ones((16,16),np.uint8)
+    # I_ro = cv2.erode(img, kernel = se, iterations = 1)
+    # I_ro_c = cv2.bitwise_not(I_ro)
+
+    # I_rc = cv2.bitwise_and(I_ro_c, I_ro_c, mask = cv2.bitwise_not(cv2.dilate(I_ro, kernel = se, iterations = 1)))
+
+    # plt.imshow(I_rc)
+    # plt.colorbar()
