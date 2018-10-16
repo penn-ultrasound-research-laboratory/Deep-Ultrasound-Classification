9f20b922c01f1e882e804dbb66003e4e0d831797
goodmattg <mattgoodman13@gmail.com>
2018-10-15 22:05:47 -0700

    algorithm working on single example using contour search and rectangle bounding. Needs verification

--- utilities/AutomaticSegmentation.py
+++ utilities/AutomaticSegmentation.py
@@ -26,11 +26,8 @@ def __gaussian_filter(image):
 
     return blur
 
-
 def __linear_normalization(image):
     lbound05, ubound95 = np.percentile(image, (5, 95))
-    print("5th Percentile: {}".format(lbound05))
-    print("95th Percentile: {}".format(ubound95))
 
     return np.piecewise(image, [
         image <= lbound05, 
@@ -51,8 +48,6 @@ def __enhance_hypoechoic_regions(image):
     else:
         z_b = (z_a + z_c * (1-SN)) / 2
 
-    print("z_a: {} | z_b: {} | z_c: {} | SN: {}".format(z_a, z_b, z_c, SN))
-
     return np.piecewise(image.astype(float), [
         image <= z_a,
         (image > z_a)&(image <= z_b),
@@ -66,10 +61,6 @@ def __enhance_hypoechoic_regions(image):
     
 def __get_reference_point(image, max_iters=100, eps=2):
 
-    plt.imshow(image)
-    plt.colorbar()
-    plt.show()
-
     # Watch out that this doesn't blow up if max_iters >>> 
     weight_dot_image_mat = np.empty((max_iters,) + image.shape)
     
@@ -79,51 +70,31 @@ def __get_reference_point(image, max_iters=100, eps=2):
     WB = np.empty((max_iters, 4))
     
     M, N = image.shape
-    row_ind, col_ind = np.indices(image.shape)
-
+    ind = np.indices(image.shape)
+    row_ind = ind[0]
+    col_ind = ind[1]
+        
     weight_dot_image_mat[0] = np.multiply(np.ones(image.shape), image)
 
     C[0] = np.array([M // 2, N // 2])
 
-    print("Initial Center: ({}, {})".format(C[0, 0], C[0, 1]))
-
     # THESE MIGHT BE THE WRONG INITIAL VALUES
     WB[0] = np.array([0.0, 0.0, 0.0 ,0.0])
-
-    print("C shape: {}".format(C.shape))
-    print("weight_dot_image_mat shape: {}".format(weight_dot_image_mat.shape))
-    print("WB shape: {}".format(WB.shape))
-    print("M: {}".format(M))
-    print("N: {}".format(N))
 
     for it in range(1, max_iters):
         # compute C_i. Update C[i] <- C_i
         w_multi_prod = np.prod(weight_dot_image_mat[:it], axis=0)
-        print("w_multi_prod shape: {}".format(w_multi_prod.shape))
-
-        plt.imshow(w_multi_prod)
-        plt.colorbar()
-        plt.show()
-
 
         # Normalization constant
         nc = np.sum(w_multi_prod.flatten())
-        print("nc: {}".format(nc))
         
         # This may be correct
         C_i_c = np.sum(np.sum(np.multiply(col_ind, w_multi_prod).flatten() / nc))
         C_i_r = np.sum(np.sum(np.multiply(row_ind, w_multi_prod).flatten() / nc))
 
-        # THIS IS WRONG!!! 
-        # C_i_c = np.sum(np.prod(np.multiply(w_multi_prod, col_ind), axis=0).flatten()  / nc) 
-        # C_i_r = np.sum(np.prod(np.multiply(w_multi_prod, row_ind), axis=0).flatten() / nc) 
-
-        print("C: ({}, {})".format(C_i_r, C_i_c))
-
         C[it] = np.array([C_i_r, C_i_c])
 
         if np.linalg.norm(C[it] - C[it-1]) < eps:
-            print("Center change less than epsilon")
             return C[it]
 
         ## Update bounds
@@ -138,46 +109,31 @@ def __get_reference_point(image, max_iters=100, eps=2):
         if C[it][0] - C[it-1][0] > 0:
             WB[it, 2] = C[it][0] - C[it-1][0]
             WB[it, 3] =  WB[it-1, 3]
-        else: 
+        else:
             WB[it, 2] = WB[it-1, 2]
             WB[it, 3] = C[it-1][0] - C[it][0]
 
-        # BOUNDS ARE POSSIBLY GOING TO WORK
-        print("New bounds: {}".format(WB[it]))
-
-        # I THINK THIS IS WRONG!!!!  
         # Update weighting function
-        row_weight_update = np.piecewise(row_ind, [
-            (row_ind < WB[it, 2])|(row_ind > M - WB[it, 3]) 
+        row_weight_update = np.piecewise(row_ind.astype(float), [
+            (row_ind > M - WB[it, 3])|(row_ind < WB[it, 2]) 
         ], [
             0.0,
-            lambda y: ((y - WB[it, 2])*(M - WB[it, 3] - y)) / ((M - WB[it, 2] - WB[it, 3]) / 2)**2
+            lambda y: ((y - WB[it, 2])*(M - WB[it, 3] - y)) / (((M - WB[it, 3] - WB[it, 2]) / 2)**2)
         ])
 
-        col_weight_update = np.piecewise(col_ind, [
-            (col_ind < WB[it, 0])|(row_ind > N - WB[it, 1]) 
+        col_weight_update = np.piecewise(col_ind.astype(float), [
+            (col_ind > N - WB[it, 1])|(col_ind < WB[it, 0]) 
         ], [
             0.0,
-            lambda x: ((x - WB[it, 0])*(N - WB[it, 1] - x)) / ((N - WB[it, 0] - WB[it, 1]) / 2)**2
+            lambda x: ((x - WB[it, 0])*(N - WB[it, 1] - x)) / (((N - WB[it, 1] - WB[it, 0]) / 2)**2)
         ])
 
         weight_dot_image_mat[it] = np.multiply(image, np.multiply(row_weight_update, col_weight_update))
-        
+
     return C[max_iters]
-
-
-def get_ROI(image):
-    blur = __gaussian_filter(image)
-    normalized = __linear_normalization(blur)
-    enhanced = __enhance_hypoechoic_regions(normalized)
-    reference_point = __get_reference_point(enhanced)
-
-    return reference_point, enhanced
-
 
 def __unit_flat_kernel(p_diff):
     return 1.0 if p_diff <= 1.0 else 0.0
-
 
 def __get_seed_point(image, rp, nd=12, h=12, eps=2, max_iters=100):
     
@@ -217,10 +173,18 @@ def __get_seed_point(image, rp, nd=12, h=12, eps=2, max_iters=100):
         max_crit[d] = nc
         post_cands[d] = p
         
-    i = np.argmax(max_crit)
-    max_p = post_cands[i, :]
+    max_p = post_cands[np.argmax(max_crit), :]
     
     return max_p, post_cands
+
+def get_ROI(image):
+    blur = __gaussian_filter(image)
+    normalized = __linear_normalization(blur)
+    enhanced = __enhance_hypoechoic_regions(normalized)
+    ref_pt = __get_reference_point(enhanced)
+    seed_pt, post_cands = __get_seed_point(enhanced, ref_pt)
+
+    return (seed_pt, ref_pt, enhanced)
 
 
 if __name__ == "__main__":
