d67541c37c8e7ff42dcd42454a023cb7b2ac4d9c
goodmattg <mattgoodman13@gmail.com>
2019-02-18 18:18:03 -0800

    switched everything to binary crossentropy. added local training feature. Added local config

--- src/models/vgg16.py
+++ src/models/vgg16.py
@@ -1,5 +1,5 @@
 from keras.applications.vgg16 import VGG16
-from keras.layers import Dense
+from keras.layers import Activation, Dropout, Flatten, Dense, GlobalAveragePooling2D
 from keras.models import Model
 
 
@@ -7,17 +7,22 @@ def get_model(config):
     model = VGG16(
         include_top=False,
         weights='imagenet',
-        input_shape=config.input_shape,
-        pooling=config.pooling)
+        input_shape=config.input_shape)
 
     # Connect output pooling to dense layer with sigmoid activation
     # Assumes binary crossentropy loss function
-    model = Model(
-        inputs=model.inputs, outputs=Dense(
-            2,
-            input_shape=(512,),
-            activation="sigmoid",
-            name="predictions")(model.layers[-1].output)
-    )
+    # model = Model(
+    #     inputs=model.inputs, outputs=Dense(
+    #         2,
+    #         input_shape=(512,),
+    #         activation="sigmoid",
+    #         name="predictions")(model.layers[-1].output)
+    # )
 
-    return model
+    x = model.get_layer('block5_conv3').output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1, activation='sigmoid')(x)
+
+    return Model(inputs=model.input, outputs=x)
