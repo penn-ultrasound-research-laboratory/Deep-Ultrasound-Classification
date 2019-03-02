4b4a9c2a924f0250f2ba8f36d1ca087e3a3fe472
goodmattg <mattgoodman13@gmail.com>
2019-03-01 21:29:10 -0800

    switching to default model name

--- src/trainer/task.py
+++ src/trainer/task.py
@@ -35,7 +35,7 @@ def train_model(args):
     JOB_DIR = default_none(args.job_dir, ".")
     LOGS_PATH = "{0}/logs".format(JOB_DIR)
     CONFIG_FILE = default_none(args.config, DEFAULT_CONFIG)
-    MODEL_FILE = "{0}.h5".format(CONFIG_FILE)
+    MODEL_FILE = "model.h5"
     GC_MODEL_SAVE_PATH = "{0}/model/{1}".format(JOB_DIR, MODEL_FILE)
 
     with tf.device('/device:GPU:0'):
